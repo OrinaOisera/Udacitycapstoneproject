647ff43c5347ad8bb95997289732bcd5d7436461
OrinaOisera <orinamark22@gmail.com>
2020-09-14 23:30:19 +0000

    docker image uploaded to hub

--- app-run/docker-run.sh
+++ app-run/docker-run.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t capstone-project .
+docker build  --tag=capstone-project .
 
 # Step 2: 
 # List docker images
-docker images
+docker image ls
 
 # Step 3: 
 # Run app
-docker run -p 80:80 landing-page
+docker run -p 80:80 capstone-project
